9f613a03d765a797370733ef9fb957982eee332e
abjklk <abjklk@gmail.com>
2020-04-11 16:56:52 +0530

    mlaburi

--- config/database.js
+++ config/database.js
@@ -1,4 +1,5 @@
 module.exports = {
-	database: 'mongodb+srv://abjklk:colonydb@cluster0-wcdm9.gcp.mongodb.net/colonydb?retryWrites=true&w=majority',
+	// database: 'mongodb+srv://abjklk:colonydb@cluster0-wcdm9.gcp.mongodb.net/colonydb?retryWrites=true&w=majority',
+	database: process.env.MONGOLAB_URI || 'mongodb://127.0.0.1:27017/colonydb',
 	secret: 'gaurav',
 }
